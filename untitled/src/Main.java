70489cefb646234d55de6aafe8b52d10da8f7cad
wellerson <wellersonjava@gmail.com>
2022-12-25 22:13:45 -0300

    Criação dos projetos das aulas do dio em Java

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String meuNome = "wellerson";
+
+        int anoFabricacao = 2022;
+
+        boolean verdadeira = true;
     }
 }
