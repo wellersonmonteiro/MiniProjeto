9b769cace1d0799c0eeba09fd4c35d2d2fa4b7fb
wellerson <wellersonjava@gmail.com>
2023-01-03 17:49:35 -0300

    Adição dos operadores

---
+++ untitled/src/edu/wellerson/OperadoresRelacionais.java
@@ -0,0 +1,12 @@
+package edu.wellerson;
+
+public class OperadoresRelacionais {
+    public static void main(String[] args) {
+        String nomeUm = "Gleyson";
+        String nomeDois = "Gleyso";
+
+        boolean nomes = nomeUm.equals(nomeDois);
+
+        System.out.println(nomes);
+    }
+}
---
+++ untitled/src/edu/wellerson/OperadoresTernarios.java
@@ -0,0 +1,22 @@
+package edu.wellerson;
+
+public class OperadoresTernarios {
+    public static void main(String[] args) {
+
+        int a,b;
+        a = 5;
+        b = 6;
+        /** String resultado = "";
+        if(a==b){
+            resultado = "true";
+
+        }
+        else {
+            resultado = "false";
+        } */
+        String resultado = a==b ? "verdadeiro" : "Falso";
+
+        System.out.print(resultado);
+
+    }
+}
